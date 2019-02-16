653af649c26f61001294c54ffa6aebc11f7443af
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-02-16 00:01:54 +0000

    Human-corrected luma grayscale function implemented.

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -5,7 +5,7 @@ use image::{GenericImage, DynamicImage, ImageBuffer, GenericImageView};
 fn main() {
     let img = image::open("test.JPG").unwrap();
 
-    let filtered_img = photon::threshold(img, 210);
+    let filtered_img = photon::grayscale(img);
     
     // Write the contents of this image in PNG format.
     filtered_img.save("test.png").unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@ pub fn threshold(mut img: DynamicImage, threshold: u32) -> DynamicImage {
     return img;
 }
 
-fn grayscale(mut img: DynamicImage) -> DynamicImage {
+pub fn grayscale(mut img: DynamicImage) -> DynamicImage {
     let (width, height) = img.dimensions();
 
     for x in 0..width {
@@ -59,7 +59,30 @@ fn grayscale(mut img: DynamicImage) -> DynamicImage {
     return img;
 }
 
-fn inc_brightness(mut img: DynamicImage, brightness: u8) -> DynamicImage {
+pub fn grayscale_human_corrected(mut img: DynamicImage) -> DynamicImage {
+    let (width, height) = img.dimensions();
+
+    for x in 0..width {
+        for y in 0..height {
+            let mut px = img.get_pixel(x, y);
+            let (r_val, g_val, b_val) = (px.data[0] as f32, px.data[1] as f32, px.data[2] as f32);
+
+            let mut avg = (r_val * 0.3 + g_val * 0.59 + b_val * 0.11);
+            
+            if (avg >= 255.0) {
+                avg = 255.0
+            }
+            
+            px.data[0] = avg as u8;
+            px.data[1] = avg as u8;
+            px.data[2] = avg as u8;
+            img.put_pixel(x, y, px);
+        }
+    }
+    return img;
+}
+
+pub fn inc_brightness(mut img: DynamicImage, brightness: u8) -> DynamicImage {
     let (width, height) = img.dimensions();
 
     for x in 0..width {
@@ -92,6 +115,11 @@ fn inc_brightness(mut img: DynamicImage, brightness: u8) -> DynamicImage {
 }
 
 pub mod channels {
+    extern crate image;
+    extern crate rand;
+    use image::{GenericImage, DynamicImage, ImageBuffer, GenericImageView};
+    use image::Pixel;
+    use rand::Rng;
     pub fn alter_channel(mut img: DynamicImage, channel: usize, offset: u8) -> DynamicImage {
         let (width, height) = img.dimensions();
         let mut rng = rand::thread_rng();
@@ -111,75 +139,110 @@ pub mod channels {
         return img;
     }
 
-    fn alter_red_channel(mut img: DynamicImage, offset: u8) -> DynamicImage {
+    pub fn alter_red_channel(mut img: DynamicImage, offset: u8) -> DynamicImage {
         let res_img = alter_channel(img, 0, offset);
         return res_img;
     }
 
-    fn alter_green_channel(mut img: DynamicImage, offset: u8) -> DynamicImage {
+    pub fn alter_green_channel(mut img: DynamicImage, offset: u8) -> DynamicImage {
         let res_img = alter_channel(img, 1, offset);
         return res_img;
     }
 
-    fn alter_blue_channel(mut img: DynamicImage, offset: u8) -> DynamicImage {
+    pub fn alter_blue_channel(mut img: DynamicImage, offset: u8) -> DynamicImage {
         let res_img = alter_channel(img, 2, offset);
         return res_img;
     }
+
+    pub fn alter_two_channels(mut img: DynamicImage, channel1: usize, offset1: u8, channel2: usize, offset2: u8) -> DynamicImage {
+        let (width, height) = img.dimensions();
+
+        for x in 0..width {
+            for y in 0..height {
+                let mut px = img.get_pixel(x, y);
+                if px.data[channel1] <= 255 - offset1 {
+                    px.data[channel1] += offset1;
+                }
+                else {
+                    px.data[channel1] = 255;
+                }
+                
+                if px.data[channel2] <= 255 - offset2 {
+                    px.data[channel2] += offset2;
+                }
+                else {
+                    px.data[channel2] = 255
+                }
+                img.put_pixel(x, y, px);
+            }
+        }
+        return img;
+    }
+
 }
 
 pub mod filters {
+    extern crate image;
+    use image::{GenericImage, DynamicImage, ImageBuffer, GenericImageView};
+    use image::Pixel;
+
     // In-built image filters
     pub fn oceanic(mut img: DynamicImage) -> DynamicImage {
-        let filtered_img = alter_two_channels(img, 1, 9, 2, 173);
+        let filtered_img = crate::channels::alter_two_channels(img, 1, 9, 2, 173);
         return filtered_img;
     }
 
     pub fn islands(mut img: DynamicImage) -> DynamicImage {
-        let filtered_img = alter_two_channels(img, 1, 24, 2, 95);
+        let filtered_img = crate::channels::alter_two_channels(img, 1, 24, 2, 95);
         return filtered_img;
     }
 
     pub fn marine(mut img: DynamicImage) -> DynamicImage {
-        let filtered_img = alter_two_channels(img, 1, 14, 2, 119);
+        let filtered_img = crate::channels::alter_two_channels(img, 1, 14, 2, 119);
         return filtered_img;
     }
 
     pub fn seagreen(mut img: DynamicImage) -> DynamicImage {
-        let filtered_img = alter_two_channels(img, 1, 68, 2, 62);
+        let filtered_img = crate::channels::alter_two_channels(img, 1, 68, 2, 62);
         return filtered_img;
     }
 
     pub fn flagblue(mut img: DynamicImage) -> DynamicImage {
-        let filtered_img = alter_blue_channel(img, 131);
+        let filtered_img = crate::channels::alter_blue_channel(img, 131);
         return filtered_img;
     }
 
     pub fn diamante(mut img: DynamicImage) -> DynamicImage {
-        let filtered_img = alter_two_channels(img, 1, 82, 2, 87);
+        let filtered_img = crate::channels::alter_two_channels(img, 1, 82, 2, 87);
         return filtered_img;
     }
 
     pub fn liquid(mut img: DynamicImage) -> DynamicImage {
-        let filtered_img = alter_two_channels(img, 1, 10, 2, 75);
+        let filtered_img = crate::channels::alter_two_channels(img, 1, 10, 2, 75);
         return filtered_img;
     }
 }
 
 pub mod noise {
-    fn add_noise(mut img: DynamicImage, offset: u8) -> DynamicImage {
-        // Add Gaussian Noise Sample with offset specified by the user.
-        let (width, height) = img.dimensions();
+    extern crate image;
+    extern crate rand;
+    use image::{GenericImage, DynamicImage, ImageBuffer, GenericImageView};
+    use image::Pixel;
+    use rand::Rng;
+    // pub fn add_noise(mut img: DynamicImage, offset: u8) -> DynamicImage {
+    //     // Add Gaussian Noise Sample with offset specified by the user.
+    //     let (width, height) = img.dimensions();
 
-        for x in 0..width {
-            for y in 0..height {
-                let px = img.get_pixel(x, y).map(|ch| if ch <= 255 - offset { ch + offset } else { 255});
-                img.put_pixel(x, y, px);
-        }
-        }
-        return img;
-    }
+    //     for x in 0..width {
+    //         for y in 0..height {
+    //             let px = img.get_pixel(x, y).map(|ch| if ch <= 255 - offset { ch + offset } else { 255});
+    //             img.put_pixel(x, y, px);
+    //     }
+    //     }
+    //     return img;
+    // }
 
-    fn add_noise_rand(mut img: DynamicImage) -> DynamicImage {
+    pub fn add_noise_rand(mut img: DynamicImage) -> DynamicImage {
         // Add Gaussian Noise Sample by including a random offset to each channel in each pixel.
         let (width, height) = img.dimensions();
         let mut rng = rand::thread_rng();
@@ -193,4 +256,31 @@ pub mod noise {
         }
         return img;
     } 
+}
+
+pub mod effects {
+    extern crate image;
+    extern crate rand;
+    use image::{GenericImage, DynamicImage, ImageBuffer, GenericImageView};
+    use image::Pixel;
+    use rand::Rng;
+    
+    pub fn offset(mut img: DynamicImage) -> DynamicImage {
+        let (width, height) = img.dimensions();
+        let mut rng = rand::thread_rng();
+
+        for x in 0..width {
+            for y in 0..height {
+                let offset = rng.gen_range(0, 150);
+                let mut px = img.get_pixel(x, y);
+
+                if x + 10 < width - 1 && y + 10 < height - 1  {
+                    let offset_px = img.get_pixel(x + 10, y + 10);
+                    px = offset_px;
+                }
+
+        }
+    }
+    return img;
+    }
 }
