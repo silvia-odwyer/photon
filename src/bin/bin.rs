14081f439ede054e5bdac4b682a980ca7d305865
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-03-26 19:49:29 +0000

    Benchmarking/timing the processing of images included in the binary.

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,13 +1,27 @@
 extern crate photon;
-use photon::effects::Rgb;
+extern crate time;
+use time::PreciseTime;
+// use photon::effects::Rgb;
 
 fn main() {
     let img = photon::helpers::open_image("background3.JPG");
-    // let color_a: Rgb = Rgb {r: 120, g: 100, b: 30 };
-    // let color_b: Rgb = Rgb {r: 110, g: 30, b: 90 };
     
-    let filtered_img = photon::conv::emboss(img);
+    let filtered_img = photon::conv::sobel_vertical(img);
     
     // Write the contents of this image in PNG format.
-    photon::helpers::save_image(filtered_img, "new_image.PNG")
+    photon::helpers::save_image(filtered_img, "new_image.PNG");
+}
+
+fn testDuration() {
+    let start = PreciseTime::now();
+    let img = photon::helpers::open_image("background3.JPG");
+    
+    let filtered_img = photon::conv::sobel_vertical(img);
+    
+    // Write the contents of this image in PNG format.
+    photon::helpers::save_image(filtered_img, "new_image.PNG");
+
+    let end = PreciseTime::now();
+
+    println!("Took {} seconds to process image.", start.to(end));
 }
