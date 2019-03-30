1971a85101a618b15a7b8182b984dfc595c3af88
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-03-30 18:18:21 +0000

    Reduced wait time before preloader fades.
    Former-commit-id: d3b5fc79ce16e3c499e91a10d417150482c35e47

--- docs/preloader.js
+++ docs/preloader.js
@@ -1,4 +1,4 @@
 $(window).load(function() {
-	$(".loader").delay(3000).fadeOut("slow");
-  $("#preloaderBackground").delay(3000).fadeOut("slow");
+	$(".loader").delay(1000).fadeOut("slow");
+  $("#preloaderBackground").delay(1000).fadeOut("slow");
 })
