ee04eeb4463241558a389db732fa11da33f005c0
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2019-05-26 23:03:37 +0100

    Cleaned up code to maximise code re-use.
    Former-commit-id: e3954adfc5150121a1322c0428c6babbd2607535

--- src/correction.rs
+++ src/correction.rs
@@ -2,71 +2,110 @@ extern crate image;
 extern crate rand;
 extern crate num;
 use image::{DynamicImage, GenericImageView};
-use palette::{Hsl, Lch, Shade, Pixel, Saturate, Srgb, Hue, Hsv, Hwb, Lab, Xyz, Yxy};
+use palette::{Hsl, Lch, Shade, Pixel, Saturate, Srgb, Hue, Hsv};
 
-use image::GenericImage;
-
-
-// Increase the image's saturation by converting each pixel's colour to the HSL colour space
-// and increasing the colour's saturation. 
-// The level must be from 0 to 1 in floating-point, `f32` format.
-// Increasing saturation by 80% would be represented by a level of 0.8
-pub fn saturate_hsl(img: DynamicImage, level: f32) -> DynamicImage {
+// Image manipulation effects in the LCh colour space
+pub fn lch(img: DynamicImage, mode: &'static str, amt: f32) -> DynamicImage {
+   
+    let (width, height) = img.dimensions();
     let mut img = img.to_rgb();
-    {
+    for x in 0..width {
+        for y in 0..height {
+            let px = img.get_pixel(x, y);
 
-        let (width, height) = img.dimensions();
-        for x in 0..width {
-            for y in 0..height {
-                let px_data = img.get_pixel(x, y).data;
+            let px_data = img.get_pixel(x, y).data;
+            let lch_colour: Lch = Srgb::from_raw(&px_data)
+                .into_format()
+                .into_linear()
+                .into();
 
-                let hsl_colour: Hsl = Srgb::from_raw(&px_data)
-                    .into_format()
-                    .into_linear()
-                    .into();
+            let new_color = match mode {
+                // Match a single value
+                "desaturate" => lch_colour.desaturate(amt),
+                "saturate" => lch_colour.saturate(amt),
+                "lighten" => lch_colour.lighten(amt), 
+                "darken" => lch_colour.darken(amt),
+                _ => lch_colour.saturate(amt),
+            };
+            
+            img.put_pixel(x, y, image::Rgb {
+                data: Srgb::from_linear(new_color.into()).into_format().into_raw()
+            });
 
-                let saturated = hsl_colour.saturate(level);
-                img.put_pixel(x, y, image::Rgb {
-                    data: Srgb::from_linear(saturated.into()).into_format().into_raw()
-                });
             }
         }
-    }
+    
     let dynimage = image::ImageRgb8(img);
     return dynimage;
 }
 
-// Increase the image's saturation by converting each pixel's colour to the LCh colour space
-// and increasing the colour's saturation. 
-// The level must be from 0 to 1 in floating-point, `f32` format.
-// Increasing saturation by 80% would be represented by a level of 0.8
-pub fn saturate_lch(img: DynamicImage, level: f32) -> DynamicImage {
-    let mut img = img.to_rgb();
-     {
-        let (width, height) = img.dimensions();
+// Image manipulation effects in the HSL colour space
+// The function logic is kept separate from other colour spaces for now, 
+// since other HSL-specific logic may be implemented here, which isn't available in other colour spaces
+pub fn hsl(img: DynamicImage, mode: &'static str, amt: f32) -> DynamicImage {
+    let mut img  = img.to_rgb();
+    let (width, height) = img.dimensions();
         for x in 0..width {
             for y in 0..height {
                 let px_data = img.get_pixel(x, y).data;
 
-                let lch_colour: Lch = Srgb::from_raw(&px_data)
-                    .into_format()
-                    .into_linear()
-                    .into();
+                let colour = Srgb::from_raw(&px_data).into_format();
 
-                let saturated = lch_colour.saturate(level);
+                let hsl_colour = Hsl::from(colour);
+                
+                let new_color = match mode {
+                    // Match a single value
+                    "desaturate" => hsl_colour.desaturate(amt),
+                    "saturate" => hsl_colour.saturate(amt),
+                    "lighten" => hsl_colour.lighten(amt), 
+                    "darken" => hsl_colour.darken(amt),
+                    _ => hsl_colour.saturate(amt),
+                };
+
                 img.put_pixel(x, y, image::Rgb {
-                    data: Srgb::from_linear(saturated.into()).into_format().into_raw()
+                    data: Srgb::from_linear(new_color.into()).into_format().into_raw()
                 });
             }
         }
-    }
+
     let dynimage = image::ImageRgb8(img);
     return dynimage;
+}
 
+pub fn hsv(img: DynamicImage, mode: &'static str, amt: f32) -> DynamicImage {
+    let mut img  = img.to_rgb();
+
+    let (width, height) = img.dimensions();
+
+        for x in 0..width {
+            for y in 0..height {
+                let px_data = img.get_pixel(x, y).data;
+
+                let color = Srgb::from_raw(&px_data).into_format();
+
+                let hsv_colour = Hsv::from(color);
+
+                let new_color = match mode {
+                    // Match a single value
+                    "desaturate" => hsv_colour.desaturate(amt),
+                    "saturate" => hsv_colour.saturate(amt),
+                    "lighten" => hsv_colour.lighten(amt), 
+                    "darken" => hsv_colour.darken(amt),
+                    _ => hsv_colour.saturate(amt),
+                };
+
+                img.put_pixel(x, y, image::Rgb {
+                    data: Srgb::from_linear(new_color.into()).into_format().into_raw()
+                });
+            }
+        }
+
+    let dynimage = image::ImageRgb8(img);
+    return dynimage;
 }
 
 // Shift hue by a specified number of degrees in the HSL colour space.
-pub fn hue_rotate_hsl(mut img: DynamicImage, degrees: f32) -> DynamicImage {
+pub fn hue_rotate_hsl(img: DynamicImage, degrees: f32) -> DynamicImage {
     let mut img  = img.to_rgb();
 
        let (width, height) = img.dimensions();
@@ -88,7 +127,7 @@ pub fn hue_rotate_hsl(mut img: DynamicImage, degrees: f32) -> DynamicImage {
 }
 
 // Shift hue by a specified number of degrees in the LCh colour space.
-pub fn hue_rotate_lch(mut img: DynamicImage, degrees: f32) -> DynamicImage {
+pub fn hue_rotate_lch(img: DynamicImage, degrees: f32) -> DynamicImage {
     let mut img  = img.to_rgb();
 
     let (width, height) = img.dimensions();
@@ -110,141 +149,53 @@ pub fn hue_rotate_lch(mut img: DynamicImage, degrees: f32) -> DynamicImage {
     return dynimage;
 }
 
+// Increase the image's saturation by converting each pixel's colour to the HSL colour space
+// and increasing the colour's saturation. 
+// The level must be from 0 to 1 in floating-point, `f32` format.
+// Increasing saturation by 80% would be represented by a level of 0.8
+pub fn saturate_hsl(img: DynamicImage, level: f32) -> DynamicImage {
+    return hsl(img, "saturate", level);
+}
+
+// Increase the image's saturation by converting each pixel's colour to the LCh colour space
+// and increasing the colour's saturation. 
+// The level must be from 0 to 1 in floating-point, `f32` format.
+// Increasing saturation by 80% would be represented by a level of 0.8
+pub fn saturate_lch(img: DynamicImage, level: f32) -> DynamicImage {
+    return lch(img, "saturate", level);
+}
+
+pub fn saturate_hsv(img: DynamicImage, level: f32) -> DynamicImage {
+    return hsv(img, "saturate", level);
+}
+
 // Lighten a colour by a specified amount in the LCh colour space.
-pub fn lighten_lch(mut img: DynamicImage, level: f32) -> DynamicImage {
-    let mut img  = img.to_rgb();
-
-    let (width, height) = img.dimensions();
-
-        for x in 0..width {
-            for y in 0..height {
-                let px_data = img.get_pixel(x, y).data;
-
-                let color = Srgb::from_raw(&px_data).into_format();
-
-                let lightened = Lch::from(color).lighten(0.1);
-                img.put_pixel(x, y, image::Rgb {
-                    data: Srgb::from_linear(lightened.into()).into_format().into_raw()
-                });
-            }
-        }
-
-    let dynimage = image::ImageRgb8(img);
-    return dynimage;
+pub fn lighten_lch(img: DynamicImage, level: f32) -> DynamicImage {
+    return lch(img, "lighten", level);
 }
 
 // Lighten a colour by a specified amount in the HSL colour space.
-pub fn lighten_hsl(mut img: DynamicImage, level: f32) -> DynamicImage {
-    let mut img  = img.to_rgb();
-
-    let (width, height) = img.dimensions();
-
-        for x in 0..width {
-            for y in 0..height {
-                let px_data = img.get_pixel(x, y).data;
-
-                let color = Srgb::from_raw(&px_data).into_format();
-
-                let lightened = Hsl::from(color).lighten(0.1);
-                img.put_pixel(x, y, image::Rgb {
-                    data: Srgb::from_linear(lightened.into()).into_format().into_raw()
-                });
-            }
-        }
-
-    let dynimage = image::ImageRgb8(img);
-    return dynimage;
+pub fn lighten_hsl(img: DynamicImage, level: f32) -> DynamicImage {
+    return hsl(img, "lighten", level);
 }
 
 // Lighten a colour by a specified amount in the HSV colour space.
-pub fn lighten_hsv(mut img: DynamicImage, level: f32) -> DynamicImage {
-    let mut img  = img.to_rgb();
-
-    let (width, height) = img.dimensions();
-
-        for x in 0..width {
-            for y in 0..height {
-                let px_data = img.get_pixel(x, y).data;
-
-                let color = Srgb::from_raw(&px_data).into_format();
-
-                let lightened = Hsv::from(color).lighten(0.1);
-                img.put_pixel(x, y, image::Rgb {
-                    data: Srgb::from_linear(lightened.into()).into_format().into_raw()
-                });
-            }
-        }
-
-    let dynimage = image::ImageRgb8(img);
-    return dynimage;
+pub fn lighten_hsv(img: DynamicImage, level: f32) -> DynamicImage {
+    return hsv(img, "lighten", level);
 }
 
 
 // Darken the image's colours by a specified amount in the LCh colour space.
-pub fn darken_lch(mut img: DynamicImage, level: f32) -> DynamicImage {
-    let mut img  = img.to_rgb();
-
-    let (width, height) = img.dimensions();
-
-        for x in 0..width {
-            for y in 0..height {
-                let px_data = img.get_pixel(x, y).data;
-
-                let color = Srgb::from_raw(&px_data).into_format();
-
-                let darkened = Lch::from(color).darken(0.1);
-                img.put_pixel(x, y, image::Rgb {
-                    data: Srgb::from_linear(darkened.into()).into_format().into_raw()
-                });
-            }
-        }
-
-    let dynimage = image::ImageRgb8(img);
-    return dynimage;
+pub fn darken_lch(img: DynamicImage, level: f32) -> DynamicImage {
+    return lch(img, "darken", level);
 }
 
 // Darken the image's colours by a specified amount in the HSL colour space.
-pub fn darken_hsl(mut img: DynamicImage, level: f32) -> DynamicImage {
-    let mut img  = img.to_rgb();
-
-    let (width, height) = img.dimensions();
-
-        for x in 0..width {
-            for y in 0..height {
-                let px_data = img.get_pixel(x, y).data;
-
-                let color = Srgb::from_raw(&px_data).into_format();
-
-                let darkened = Hsl::from(color).darken(0.1);
-                img.put_pixel(x, y, image::Rgb {
-                    data: Srgb::from_linear(darkened.into()).into_format().into_raw()
-                });
-            }
-        }
-
-    let dynimage = image::ImageRgb8(img);
-    return dynimage;
+pub fn darken_hsl(img: DynamicImage, level: f32) -> DynamicImage {
+    return hsl(img, "darken", level);
 }
 
 // Darken the image's colours by a specified amount in the HSV colour space.
-pub fn darken_hsv(mut img: DynamicImage, level: f32) -> DynamicImage {
-    let mut img  = img.to_rgb();
-
-    let (width, height) = img.dimensions();
-
-        for x in 0..width {
-            for y in 0..height {
-                let px_data = img.get_pixel(x, y).data;
-
-                let color = Srgb::from_raw(&px_data).into_format();
-
-                let darkened = Hsv::from(color).darken(0.1);
-                img.put_pixel(x, y, image::Rgb {
-                    data: Srgb::from_linear(darkened.into()).into_format().into_raw()
-                });
-            }
-        }
-
-    let dynimage = image::ImageRgb8(img);
-    return dynimage;
+pub fn darken_hsv(img: DynamicImage, level: f32) -> DynamicImage {
+    return hsv(img, "darken", level);
 }
